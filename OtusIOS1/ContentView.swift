85af0c6825684edbca0f8da44d2ebd304f817b42
Alexey Bolvonovich <alexeybolv@gmail.com>
2021-03-14 13:44:51 +0300

    feat: start app

---
+++ OtusIOS1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OtusIOS1
+//
+//  Created by Alexey Bolvonovich on 14.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OtusIOS1/OtusIOS1App.swift
@@ -0,0 +1,17 @@
+//
+//  OtusIOS1App.swift
+//  OtusIOS1
+//
+//  Created by Alexey Bolvonovich on 14.03.21.
+//
+
+import SwiftUI
+
+@main
+struct OtusIOS1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
