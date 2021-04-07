509a0ea72f6a975df64a6b6e03861ac0dadb870c
Alexey Bolvonovich <alexeybolv@gmail.com>
2021-04-07 11:35:57 +0300

    fix: return back navigation title

--- OtusIOS1/Recipes/RecipesScreenView.swift
+++ OtusIOS1/Recipes/RecipesScreenView.swift
@@ -103,7 +103,7 @@ struct RecipesScreenView: View {
                         }
                     }
                 } // List
-            }
+            }.navigationTitle("Recipes")
         }
     }
 }
